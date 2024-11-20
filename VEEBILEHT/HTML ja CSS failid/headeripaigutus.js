7b56345e3bbbfa91546eaa4dbbb5be8f643c8af4
Vezaaz <kristokontse@gmail.com>
2024-11-20 14:42:36 +0200

    Uuendus

---
+++ VEEBILEHT/HTML ja CSS failid/headeripaigutus.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    const main = document.querySelector('main');
+    const headerHeight = header.offsetHeight;
+    main.style.paddingTop = `60px`;
+});
