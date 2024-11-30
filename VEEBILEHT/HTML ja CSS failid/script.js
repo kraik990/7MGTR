962cb4b686f669ddcd7f87b5c81487fabf335541
Vezaaz <kristokontse@gmail.com>
2024-11-30 13:02:03 +0200

    uuendus

--- VEEBILEHT/HTML ja CSS failid/script.js
+++ VEEBILEHT/HTML ja CSS failid/script.js
@@ -1,25 +1,27 @@
-// Fail: scipt.js //
+// Fail: script.js //
 // Autor: Ryan (ryfarlane.com) //
 // Kuupäev: 30.08.2022 //
-// Kirjeldus: See skript tekitab tekstile fade in animatsiooni ja tuvastab kasutaja kerimist,
-// tänu millele ta ajastab fade in-i. //
+// Kirjeldus: See skript tekitab tekstile fade-in animatsiooni ja tuvastab kasutaja kerimist,
+// tänu millele ta ajastab fade-in-i. //
 
 // Valime kõik elemendid, millele on antud klass 'fade-in-up-on-scroll' //
 const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-in-up-on-scroll");
 
-// Kontrollime, kas meil on javaskript html-is väljakutsutud //
-if (elementsToFadeInUpOnScroll) {
-  // Lisame evendi tuvastaja, mis ootab kasutaja kerimist ("scroll"). //
+// Kontrollime, kas meil on javaskript HTML-is väljakutsutud //
+if (elementsToFadeInUpOnScroll.length > 0) {
+  // Lisame sündmuse kuulaja, mis ootab kasutaja kerimist ("scroll"). //
   window.addEventListener("scroll", function(event) {
 
     // Käime läbi kõik valitud elemendid //
     elementsToFadeInUpOnScroll.forEach(function(element) {
 
-      // Kui kasutaja jõuab kerimisega elemendi algusesse // 
+      // Kui kasutaja jõuab kerimisega elemendi algusesse //
       if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
-        element.classList.add("fade-in-up"); // Lisab klassi ja rakendab cssi koodis olevaid animatsioone //
+        // Lisab klassi "fade-in-up", mis käivitab CSS-i animatsiooni //
+        element.classList.add("fade-in-up");
       } else {
-        element.classList.remove("fade-in-up"); // Eemaldab klassi, element läheb algasendisse //
+        // Eemaldab klassi, kui element ei ole nähtav //
+        element.classList.remove("fade-in-up");
       }
     });
   });
