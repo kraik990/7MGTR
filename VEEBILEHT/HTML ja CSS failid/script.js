884b9e200eac972d3d2ede3354110507d0c86759
Vezaaz <kristokontse@gmail.com>
2024-11-30 12:50:40 +0200

    uuendus

--- VEEBILEHT/HTML ja CSS failid/script.js
+++ VEEBILEHT/HTML ja CSS failid/script.js
@@ -1,19 +1,26 @@
-const messageInput = document.querySelector("textarea");
-const maxLength = 200;
-const charCounter = document.createElement("div");
+// Fail: scipt.js
+// Autor: Ryan https://ryfarlane.com/index
+// Kuupäev: 30.08.2022
+// Kirjeldus: See skript tekitab tekstile fade in animatsiooni ja tuvastab kasutaja kerimist,
+// tänu millele ta ajastab fade in-i.
 
-charCounter.textContent = `0/${maxLength} tähemärki`;
-charCounter.style.fontSize = "0.9rem";
-charCounter.style.color = "gray";
-messageInput.parentNode.appendChild(charCounter);
+// Valime kõik elemendid, millele on antud klass 'fade-in-up-on-scroll'
+const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-in-up-on-scroll");
 
-messageInput.addEventListener("input", () => {
-    const currentLength = messageInput.value.length;
-    charCounter.textContent = `${currentLength}/${maxLength} tähemärki`;
+// Kontrollime, kas meil on javaskript html-is väljakutsutud
+if (elementsToFadeInUpOnScroll) {
+  // Lisame evendi tuvastaja, mis ootab kasutaja kerimist ("scroll").
+  window.addEventListener("scroll", function(event) {
 
-    if (currentLength > maxLength) {
-        charCounter.style.color = "red";
-    } else {
-        charCounter.style.color = "gray";
-    }
-});
+    // Käime läbi kõik valitud elemendid
+    elementsToFadeInUpOnScroll.forEach(function(element) {
+
+      // Kui kasutaja jõuab kerimisega elemendi algusesse
+      if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
+        element.classList.add("fade-in-up"); // Lisab klassi ja rakendab cssi koodis olevaid animatsioone
+      } else {
+        element.classList.remove("fade-in-up"); // Eemaldab klassi, element läheb algasendisse
+      }
+    });
+  });
+}
